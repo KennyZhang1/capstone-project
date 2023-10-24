15a816c724c3e5036e98f3d08c9ffa67e6828434
Ruhul Quddus <rq6gu@virginia.edu>
2023-10-24 12:57:05 -0400

    Final PCB's done.

--- Glove-Code/PicoCode/main.py
+++ Glove-Code/PicoCode/main.py
@@ -96,6 +96,10 @@ async def find_robot_pico():
     # maximise detection rate).
     async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
         async for result in scanner:
+            if result.name()!= None:
+                print("SCAN RESULT: ", result.name())
+                for id in result.services():
+                    print("\t UUIDs: ", id)
             # See if it matches our name and the environmental sensing service.
             if result.name() == "mpy-board" and _ENV_SENSE_UUID in result.services():
                 return result.device
